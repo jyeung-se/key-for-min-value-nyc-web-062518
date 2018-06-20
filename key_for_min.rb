39b849721de39e9163c5b6f7c521ff2f637f68f6
Jack Yeung <jyeung.se@gmail.com>
2018-06-20 03:05:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,3 +18,13 @@ def key_for_min_value(name_hash)
   end 
 end
 
+
+low = nil
+  low_name = nil 
+  name_hash.each do |key, value|
+    if low == nil || low > value
+      low = value
+      low_name = key
+    end
+  end
+  low_name
